c0ec7008ebb68719c98d5bd120cd1a4a43f4f80e
Nicole Lim <limxinyi.nicole@gmail.com>
2024-09-13 12:49:20 +0800

    Add mui and swagger docs

--- backend/question-service/index.js
+++ backend/question-service/index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const yaml = require("yaml");
+const fs = require("fs");
 
 const envFilePath = path.join(
   path.resolve(path.dirname(path.dirname(__dirname))),
-  ".env"
+  ".env",
 );
+
+const file = fs.readFileSync("./swagger.yml", "utf-8");
+const swaggerDocument = yaml.parse(file);
 
 dotenv.config({ path: envFilePath });
 
@@ -14,7 +20,9 @@ const app = express();
 const PORT = process.env.QUESTION_SERVICE_PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World from question-service" });
+  res.status(200).json({ message: "Hello World from question-service" });
 });
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
